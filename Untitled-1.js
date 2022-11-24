fe72d212e5dc8717327b70847e8d5ed34d0068bf
erçetin güler <ercetinguler@hotmail.com>
2022-11-24 19:37:28 +0300

    update

--- Untitled-1.js
+++ Untitled-1.js
@@ -1,2 +1,8 @@
 document.querySelector("body")
-const newLocal = document.querySelector("body")
+condo {
+	st newLocal = document.querySelector("body")
+} whiledo {
+	 (conditi
+} whiledo {
+	 (conditi
+} while (condition);on);
